2e56b80a9453945021706ea60bb4f55ed4996b24
Marko Milić <marko.milic224@gmail.com>
2020-07-13 15:27:27 +0200

    JUnit test not working

---
+++ PripremaPrakse_MarkoMilić/PripremaPrakse_MarkoMilić/src/hr/atos/praksa/markomilic/zadatak15/TestJunit.java
@@ -0,0 +1,16 @@
+package hr.atos.praksa.markomilic.zadatak15;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestJunit {
+
+	@Test
+	
+	public void testAdd() {
+		String str = "Junit is working fine";
+		assertEquals("Junit is working fine", str);
+	}
+	
+}
---
+++ PripremaPrakse_MarkoMilić/PripremaPrakse_MarkoMilić/src/hr/atos/praksa/markomilic/zadatak15/TestZaposlenikService.java
@@ -0,0 +1,70 @@
+package hr.atos.praksa.markomilic.zadatak15;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class TestZaposlenikService {
+
+	
+	
+	@Test
+	public void createZaposlenik() {
+		
+		
+		try(Connection conn = Connect.getConnection()) {
+			try(Statement stCheck = conn.createStatement()) {
+				
+				conn.setAutoCommit(false);
+				
+				stCheck.executeUpdate("DELETE FROM zaposlenik");
+				
+				ZaposlenikService zs = new ZaposlenikService();
+				int id = 6;
+				String ime = "Marko";
+				String prezime = "Milić";
+				String radnomjesto = "Osijek";
+				String oib = "12345";
+				
+				Zaposlenik zaposlenik = new Zaposlenik(id, ime, prezime, radnomjesto, oib);
+				zs.create(zaposlenik);
+				assertEquals(id, zaposlenik.getId());
+				assertEquals(ime, zaposlenik.getIme());
+				assertEquals(prezime, zaposlenik.getPrezime());
+				assertEquals(radnomjesto, zaposlenik.getRadnoMjesto());
+				assertEquals(oib, zaposlenik.getOib());
+				
+				
+				try(ResultSet rs = stCheck.executeQuery("SELECT * FROM zaposlenik")) {
+					
+					assertTrue(rs.next());
+					assertEquals(id, rs.getInt("id"));
+					assertEquals(ime, rs.getString("ime"));
+					assertEquals(prezime, rs.getString("prezime"));
+					assertEquals(radnomjesto, rs.getString("radnomjesto"));
+					assertEquals(oib, rs.getString("oib"));
+					assertFalse(rs.next());
+						
+						
+				} 
+				
+			} finally {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			fail(e.toString());
+		}
+		
+	}
+	
+	
+	
+}
